57ab1380edbe98014b66efb4ed747a77288baa94
Muhammad Balyan <mmbalyann@gmail.com>
2020-05-05 14:29:20 +0700

    add model buku besar

---
+++ application/models/bukuModel.php
@@ -0,0 +1,28 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class bukuModel extends CI_Model {
+
+	public function show($kode, $bulan, $tahun) {
+		$query 	= " SELECT  coa_id, tanggal, nama_coa, posisi, nominal, 
+						    MONTH(tanggal) bulan, YEAR(tanggal) tahun 
+					  FROM  jurnal AS a 
+					  JOIN  coa AS b 
+					    ON  a.coa_id = b.kode_coa 
+					 WHERE  coa_id = '$kode' 
+					HAVING  bulan = '$bulan' AND tahun = '$tahun' 
+				  ORDER BY  a.no ASC ";
+		return $this->db->query($query)->result_array();
+    }
+
+    public function getYear($tabel) {
+		$this->db->select('YEAR(tanggal) tahun');
+		$this->db->group_by('tahun');
+		return $this->db->get($tabel)->result_array();
+	}
+
+    public function getOne($kode, $tabel) {
+        $this->db->where('kode_coa', $kode);
+		return $this->db->get($tabel)->row_array();
+    }
+
+}
