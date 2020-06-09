4a1d943e320e67c3d0b4e07470c3c228c7760b13
Muhammad Balyan <mmbalyann@gmail.com>
2020-06-09 19:04:56 +0700

    modify view

--- application/views/transaksi/produksi/detail.php
+++ application/views/transaksi/produksi/detail.php
@@ -1,15 +1,15 @@
 <?php $this->libs->rowOpen($judul, ''); ?>
-    <div class="product-status-wrap">
-        <div id="toolbar"> 
-            <h5><?php echo "Kode produksi: ".$hasil['kode_produksi']."-".jumlahAngka($hasil['id_produksi'])."  
-            <br> Mulai produksi: ".shortdate_indo($hasil['mulai'])." <br> Selesai produksi: ".shortdate_indo($hasil['selesai']) ?></h5>
-
-        </div>
+<div class="product-status-wrap">
+    <div class="details-blog-dt blog-sig-details-dt courses-info mobile-sm-d-n">
+        <span><a href="#"><i class="fa fa-list"></i> <b>Kode Produksi:</b> <?php echo $hasil['kode_produksi'] . "-" . jumlahAngka($hasil['id_produksi']) ?></a></span>
+        <span><a href="#"><i class="far fa-calendar-alt"></i> <b>Mulai Produksi:</b> <?php echo shortdate_indo($hasil['mulai']) ?></a></span>
+        <span><a href="#"><i class="far fa-calendar-check"></i> <b>Selesai Produksi:</b> <?php echo shortdate_indo($hasil['selesai']) ?></a></span>
     </div>
+</div>
 
-    <div class="add-product">
-        <a href="<?php echo $tabel; ?>" class="Primary mg-b-10"><i class="fas fa-chevron-left" aria-hidden="true"></i>&nbsp;&nbsp;Kembali</a>
-    </div>
+<div class="add-product">
+    <a href="<?php echo $tabel; ?>" class="Primary mg-b-10"><i class="fas fa-chevron-left" aria-hidden="true"></i>&nbsp;&nbsp;Kembali</a>
+</div>
 
 <div class="sparkline13-graph">
     <div class="datatable-dashv1-list custom-datatable-overright">
