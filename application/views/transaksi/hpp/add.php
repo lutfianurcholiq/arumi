5989f44dba36a56acf65720cebdceb76a2da6869
Muhammad Balyan <mmbalyann@gmail.com>
2020-06-27 22:35:10 +0700

    add OH, biaya produksi, HPPenjualan, modify view

--- application/views/transaksi/hpp/add.php
+++ application/views/transaksi/hpp/add.php
@@ -2,10 +2,6 @@
 <br>
     <div class="sparkline13-graph">
         <form action="<?php echo $url; ?>" method="POST">
-            <?php $this->libs->inputOpen('nominal', 'required'); ?>
-                <input class="form-control" type="text" name="nominal" placeholder="Contoh: 150.000" onkeypress="return numeric(event)" value="<?php echo set_value('nominal'); ?>">
-                <?php echo form_error('nominal'); ?>
-            <?php $this->libs->inputClose(); ?>
 
             <?php $this->libs->inputOpen('bahan baku', ''); ?>
                 <input class="form-control" type="text" value="<?php echo rp($hasil['bahan_baku']) ?>" readonly>
@@ -18,6 +14,24 @@
             <?php $this->libs->inputOpen('tenaga kerja', ''); ?>
                 <input class="form-control" type="text" value="<?php echo rp($hasil['tenaga_kerja']) ?>" readonly>
             <?php $this->libs->inputClose(); ?>
+
+            <?php $this->libs->inputOpen('overhead pabrik', ''); ?>
+                <input class="form-control" type="text" value="<?php echo rp($hasil['oh']) ?>" readonly>
+            <?php $this->libs->inputClose(); ?>
+
+            <?php
+                $biaya_produksi = $hasil['bahan_baku'] + $hasil['bahan_penolong'] + $hasil['tenaga_kerja'] + $hasil['oh'];
+            ?>
+
+            <?php $this->libs->inputOpen('biaya produksi', ''); ?>
+                <input class="form-control" type="text" value="<?php echo rp($biaya_produksi) ?>" readonly>
+            <?php $this->libs->inputClose(); ?>
+
+            <?php $this->libs->inputOpen('harga pokok penjualan', 'required'); ?>
+                <input class="form-control" type="text" value="<?php echo rp($biaya_produksi) ?>" readonly>
+                <input class="form-control" type="hidden" name="nominal" value="<?php echo $biaya_produksi ?>" readonly>
+            <?php $this->libs->inputClose(); ?>
+            
 
             <input type="hidden" name="id_produksi" value="<?php echo $hasil['id_produksi'] ?>">
             
